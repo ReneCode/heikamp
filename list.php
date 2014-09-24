10959e390936b8de5ed64eaf7eceaff250e41eb4
Rene <harrymillermail@googlemail.com>
2014-09-24 15:04:12 +0200

    different list on one or more query-strings

--- list.php
+++ list.php
@@ -66,55 +66,53 @@ for ($i=0; $i<count($aTmp); $i++) {
 
 // query on all fields
 // combine the query-strings with AND
-$aFilter = array();
-$sql = "select * from tblobject WHERE ";
 $cntQuery = count($aQ);
 if ($cntQuery > 0) {
-	// where on 'stadt'
-	for ($i=0; $i<count($aQ); $i++) {
-		if ($i > 0) {
-			$sql = $sql . "AND ";
+	// show only, if there is at least one query-string
+	if ($cntQuery == 1) {
+		// only one query-string than 
+		// 1. look for 'stadt'  
+		// 2. look for the other fields (and stadt does not match)
+
+		// where on 'stadt'
+		$aFilter = array();
+		$sql = "select * from tblobject WHERE ";
+		$sql = $sql . "(stadt LIKE :q) ";
+		$aFilter = array(":q" => "%" . $sQuery . "%");
+		$sql = $sql . " order by stadt,plz,bundesland,strasse";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute($aFilter);
+		printTable($stmt);
+
+		// where in all other fields (and not 'stadt')	
+		$aFilter = array();
+		$sql = "select * from tblobject WHERE ";
+		$sql = $sql . "(not stadt LIKE :q AND (plz like :q OR bundesland like :q OR strasse like :q OR type like :q OR groesse like :q)) ";
+		$aFilter = array(":q" => "%" . $sQuery . "%");
+		$sql = $sql . " order by stadt,plz,bundesland,strasse";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute($aFilter);
+		printTable($stmt);
+	}
+	else {
+		// if more than 1 query-string
+		// than look on all fields
+		$aFilter = array();
+		$sql = "select * from tblobject WHERE ";
+		for ($i=0; $i<count($aQ); $i++) {
+			if ($i > 0) {
+				$sql = $sql . "AND ";
+			}
+			$sql = $sql . "(stadt LIKE :q$i OR plz like :q$i OR bundesland like :q$i OR strasse like :q$i OR type like :q$i OR groesse like :q$i) ";
+			$arr = array(":q$i" => "%" . $aQ[$i] . "%");
+			$aFilter = array_merge($aFilter, $arr);
 		}
-		$sql = $sql . "(stadt LIKE :q$i) ";
-		$arr = array(":q$i" => "%" . $aQ[$i] . "%");
-		$aFilter = array_merge($aFilter, $arr);
-	}
-	$sql = $sql . " order by stadt,plz,bundesland,strasse";
-	$stmt = $pdo->prepare($sql);
-	$nr = 0;
-	$stmt->execute($aFilter);
-	printTable($stmt);
-
-//	$sql = $sql . " UNION SELECT * FROM tblobject WHERE ";
-
-	$sql = "select * from tblobject WHERE ";
-	// where in all other fields (expect 'stadt')	
-	for ($i=0; $i<count($aQ); $i++) {
-		if ($i > 0) {
-			$sql = $sql . "AND ";
-		}
-		$sql = $sql . "(not stadt LIKE :q$i AND (plz like :q$i OR bundesland like :q$i OR strasse like :q$i OR type like :q$i OR groesse like :q$i)) ";
-		$arr = array(":q$i" => "%" . $aQ[$i] . "%");
-		$aFilter = array_merge($aFilter, $arr);
+		$sql = $sql . " order by stadt,plz,bundesland,strasse";
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute($aFilter);
+		printTable($stmt);
 	}
 
-	$sql = $sql . " order by stadt,plz,bundesland,strasse";
-	$stmt = $pdo->prepare($sql);
-	$nr = 0;
-	$stmt->execute($aFilter);
-	printTable($stmt);
-	/*
-	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		$nr++;
-		$stadt = $row['stadt']; 
-		$plz = $row['plz']; 
-		$bundesland = $row['bundesland']; 
-		$strasse = $row['strasse']; 
-		$objekttyp = $row['type']; 
-		$groesse = $row['groesse']; 
-		print_r("<tr class=\"objrow\"><td>$stadt</td><td>$plz</td><td>$bundesland</td><td>$strasse</td><td>$objekttyp</td><td>$groesse</td></tr>");
-	}
-	*/
 }
 ?>
 		 </table>
